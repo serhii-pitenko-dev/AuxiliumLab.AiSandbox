3da46aed5b8773abb4f4743b763424b1332de001
SergPit <sergpiten@gmail.com>
2026-02-25 16:19:12 +0100

    small refactoring

--- AiSandBox.Ai/Configuration/AiPolicy.cs
+++ AiSandBox.Ai/Configuration/AiPolicy.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AiSandBox.Ai.Configuration;
+﻿namespace AiSandBox.Ai.Configuration;
 
 /// <summary>
 /// Defines the type of neural network policy used by the RL agent.
